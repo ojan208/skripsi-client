5da6a7f0b5bbcbfc583016f2a952c30c53880a68
ojan208 <mfauzanazhima18@gmail.com>
2025-05-02 20:22:14 +0700

    fill the detail for the node config of each cluster

--- terraform/client-config.tf
+++ terraform/client-config.tf
@@ -3,18 +3,6 @@ provider "google" {
     region		= "asia-southeast2"
 }
 
-# provider "kubernetes" {
-#     alias = "gke"
-#     config_path = "~/.kube/config"
-# }
-
-# provider "helm" {
-#     alias = "helm"
-#     kubernetes {
-#       config_path = "~/.kube/config"
-#     }
-# }
-
 locals {
   cluster_regions = { for cluster in var.cluster : cluster.location => cluster}
 }
@@ -24,19 +12,14 @@ resource "google_container_cluster" "gke" {
     name = "cluster-${ each.value.location }"
     location = each.value.zone
     initial_node_count = 5
+    release_channel {
+      channel = "stable"
+    }
+
+    node_config {
+      machine_type = "e2-standard-2"
+      tags = [ "minecraft" ]
+      disk_size_gb = 10
+      disk_type = "pd-ssd"
+    }
 }
-
-# untuk automasi
-# resource "google_container_cluster" "gke" {
-#     for_each = local.clusters
-#     name = "cluster-${ each.key }"
-#     location = "${ each.value.zone }"
-
-#     # membuat 5 node untuk 5 client
-#     initial_node_count = 5
-# }
-
-# resource "helm_release" "client_bots" {
-#     name = "minecraft-bot"
-#     chart = "minecraft-chart"
-# }
